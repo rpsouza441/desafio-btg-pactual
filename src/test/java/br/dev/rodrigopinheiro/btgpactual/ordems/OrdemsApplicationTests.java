c3532e05281e32cb9abb8d424b66a6f74abe2c70
rpsouza441 <rpsouza@hotmail.co.uk>
2024-06-19 20:31:42 -0300

    first commit

---
+++ src/test/java/br/dev/rodrigopinheiro/btgpactual/ordems/OrdemsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.rodrigopinheiro.btgpactual.ordems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
